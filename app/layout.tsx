26a26a2e24da987bcc6a00967f704541dfec3f57
Levi Byte <aotlevi81@gmail.com>
2025-11-24 22:24:32 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,42 @@ export const metadata: Metadata = {
       href: siteConfig.logo.href,
       url: siteConfig.logo.href
     }
-  ]
+  ],
+  applicationName: 'Dubai Team Marketing',
+  authors: [{ name: 'Dubai Team.', url: 'https://dubaiteammktg.com/' }],
+  creator: 'Abdullah Al-Waraa',
+  keywords: ['dubai', 'dubai team', 'marketing', 'trading', 'trending', 'dubai team marketing'],
+  openGraph: {
+    type: 'website',
+    description: siteConfig.page.description,
+    title: siteConfig.page.title,
+    emails: [siteConfig.email],
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: siteConfig.page.title
+      }
+    ],
+    siteName: 'Dubai Team Marketing',
+    url: siteConfig.website
+  },
+  robots: { index: true, follow: true },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.page.title,
+    description: siteConfig.page.description,
+    site: siteConfig.website,
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: siteConfig.page.title
+      }
+    ]
+  }
 }
 
 const font = Inter({ subsets: ['latin'] })
